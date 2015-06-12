ad344ee6811d89a7f5ecb6b27149b023839506a4
Bryce910 <blanet910@yahoo.com>
2015-06-12 04:00:30 -0500

    Class Projectile
    Class Projectile

--- projectile.cpp
+++ projectile.cpp
@@ -19,23 +19,23 @@ void projectile::FaddProjectile()
 }
 void projectile::FmoveToLocation(float x, float y)
 {
-	projectile:FsetAngle(x, y);
+	this->FsetAngle(x, y);
 	this->ANGLE = this->PROJECTILE.getRotation();
 	/* Moving to the spot*/
-	unsigned int dynamicX = cos(this->ANGLE * 3.14 / 180) * this->PROJECTILESPEED;
-	unsigned int dynamicY = sin(this->ANGLE * 3.14 / 180) * this->PROJECTILESPEED;
+	 double dynamicX = cos(this->ANGLE * 3.14 / 180) * this->PROJECTILESPEED;
+	 double dynamicY = sin(this->ANGLE * 3.14 / 180) * this->PROJECTILESPEED;
 
-	this->PROJECTILE.move(dynamicX, dynamicY);
+	this->PROJECTILE.move(float(dynamicX), float(dynamicY));
 }
 void projectile::FsetAngle(float x, float y)
 {
 	sf::Vector2f curPos = sf::Vector2f(this->PROJECTILE.getGlobalBounds().left, this->PROJECTILE.getGlobalBounds().top);
 	sf::Vector2f pos = sf::Vector2f(x, y);
 
-	float pi = 3.14159265;
+	double pi = 3.14159265;
 	float dx = curPos.x - pos.x;
 	float dy = curPos.y - pos.y;
-	float rotation = (atan2(dy, dx)) * 180 / pi;
+	double rotation = (atan2(dy, dx)) * 180 / pi;
 
-	this->PROJECTILE.setRotation(rotation);
+	this->PROJECTILE.setRotation(float(rotation));
 }
